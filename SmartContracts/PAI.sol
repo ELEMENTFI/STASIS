fba91a57ba497aa5e437efc1822f271b991f9053
Prabhakaran1998 <73458527+Prabhakaran1998@users.noreply.github.com>
2021-03-30 08:09:13 +0530

    Update PAI.sol
    Changes made in _tTotal variable

--- SmartContracts/PAI.sol
+++ SmartContracts/PAI.sol
@@ -527,7 +527,7 @@ contract PAI is Context, IBEP20, Ownable {
     address[] public _rewardExcluded;
    
     uint256 private constant MAX = ~uint256(0);
-   // uint256 private _tTotal = 1000000 * 10**6 * 10**7; 
+    uint256 private _tTotal ;
     //uint256 private _rTotal = (MAX - (MAX % _tTotal));
     uint256 private _tTaxTotal;
     uint256 private _tBurnTotal;
